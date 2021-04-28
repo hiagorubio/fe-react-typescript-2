0756f9afa49f8e707f76dc59be5711d6c688b9dd
Hiago Rubio <hiago.rubio@bancointer.com.br>
2021-04-28 18:58:26 -0300

    Fix lint errors

--- src/components/molecules/HistoryStepper/index.tsx
+++ src/components/molecules/HistoryStepper/index.tsx
@@ -9,33 +9,37 @@ import { OwnProps } from './types';
 const HistoryStepper = ({ steps }: OwnProps) => {
   const classes = useStyles();
 
-  return steps.map(step => (
-    <div className={classes.root}>
-      <div className={classes.stepperArea}>
-        <span className={classes.stepperPreLine}/>
-        <span className={classes.stepperCircle}/>
-        <span className={classes.stepperPostLine}/>
-      </div>
-      <div className={classes.detailsContainer}>
-        <div>
-          <Typography className={classes.label}>
-            {step.date}
-          </Typography>
+  return (
+    <>
+      {steps.map(step => (
+      <div className={classes.root}>
+        <div className={classes.stepperArea}>
+          <span className={classes.stepperPreLine}/>
+          <span className={classes.stepperCircle}/>
+          <span className={classes.stepperPostLine}/>
         </div>
-      <div className={classes.detailsUser}>
-        <Avatar
-          src={step.avatarUrl}
-          alt={`${step.login} GitHub avatar`}
-          size={32}
-        />
-        <div className={classes.detailsTextContainer}>
-          <Typography className={classes.text}>{step.action}</Typography>
-          <Typography className={classes.text}>{step.repository}</Typography>
+        <div className={classes.detailsContainer}>
+          <div>
+            <Typography className={classes.label}>
+              {step.date}
+            </Typography>
+          </div>
+        <div className={classes.detailsUser}>
+          <Avatar
+            src={step.avatarUrl}
+            alt={`${step.login} GitHub avatar`}
+            size={32}
+          />
+          <div className={classes.detailsTextContainer}>
+            <Typography className={classes.text}>{step.action}</Typography>
+            <Typography className={classes.text}>{step.repository}</Typography>
+          </div>
+        </div>
         </div>
       </div>
-      </div>
-    </div>
-  ));
+    ))}
+  </>
+  );
 };
 
 export default HistoryStepper;
